7f0b314b0fd44c8d00ea343a486cb5bead534d30
wnxd <imiku@wnxd.me>
2018-07-12 15:05:23 +0800

    u

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,18 +16,18 @@ EOF
 
 echo "root:$ROOT_PASSWORD" | chpasswd
 
-(nohup /usr/sbin/sshd -D -e -o PermitRootLogin=yes) &
-
-# /usr/bin/ss-server -s 0.0.0.0 \
-#                    -p $SS_PORT \
-#                    -k $SS_PASSWORD \
-#                    -m $SS_METHOD \
-#                    -t $SS_TIMEOUT \
-#                    $SS_FASTOPEN \
-#                    -d $SS_DNS \
-#                    -d $SS_DNS2 \
-#                    $SS_UDP_RELAY \
-#                    $SS_ARGS \
-#                    -f /tmp/ss.pid &
-
 echo "Start Success !"
+
+(/usr/bin/ss-server -s 0.0.0.0 \
+                   -p $SS_PORT \
+                   -k $SS_PASSWORD \
+                   -m $SS_METHOD \
+                   -t $SS_TIMEOUT \
+                   $SS_FASTOPEN \
+                   -d $SS_DNS \
+                   -d $SS_DNS2 \
+                   $SS_UDP_RELAY \
+                   $SS_ARGS \
+                   -f /tmp/ss.pid) &
+
+/usr/sbin/sshd -D -e -o PermitRootLogin=yes
